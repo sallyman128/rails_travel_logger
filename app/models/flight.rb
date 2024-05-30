cec6ae209964ce0cfacc06ed5ce8fa9495fb3e75
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-05-30 19:52:19 -0400

    update model relationships

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
+  belongs_to :user
+  belongs_to :itinerary
+
   validates :flight_number, presence: true
   validates :departure_time, presence: true
   validates :arrival_time, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :flights
+  has_many :itineraries
 end
