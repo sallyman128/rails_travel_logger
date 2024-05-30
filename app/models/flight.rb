4477713eafb0f697569762d25c940257209e5dbb
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-05-30 19:44:19 -0400

    add validations for flight object

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,7 @@
 class Flight < ApplicationRecord
+  validates :flight_number, presence: true
+  validates :departure_time, presence: true
+  validates :arrival_time, presence: true
+  validates :origin, presence: true
+  validates :destination, presence: true
 end
