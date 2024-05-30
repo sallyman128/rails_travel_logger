33c9ac76b3e1fa6206b5c1b45aef050ffd1a64bb
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-05-30 19:50:54 -0400

    generated scaffold of Itinerary

---
+++ db/migrate/20240530235044_create_itineraries.rb
@@ -0,0 +1,10 @@
+class CreateItineraries < ActiveRecord::Migration[7.1]
+  def change
+    create_table :itineraries do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
