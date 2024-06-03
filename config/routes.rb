f164cb2c03d9e22aacd31e4e421ba3f95af41dc1
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-03 14:43:50 -0400

    troubleshooting logout process

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root to: "home#index"
 end
