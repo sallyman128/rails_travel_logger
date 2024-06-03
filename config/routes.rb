5515235caf10d95e12fc07103e27d1be7c682c83
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-03 13:25:46 -0400

    add root route for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   resources :itineraries
   resources :flights
@@ -9,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
