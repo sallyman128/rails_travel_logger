da9b2f0dae097b2b75bdc6a8bc98ddc4f793a79f
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-03 15:02:45 -0400

    trouble shooting logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,24 @@
-import Rails from "@rails/ujs"
+// Import Rails UJS (Unobtrusive JavaScript)
+import Rails from "@rails/ujs";
+
+// Import other JavaScript libraries or modules
+import * as ActiveStorage from "@rails/activestorage";
+import Turbolinks from "turbolinks";
+import "channels";
 
 // Initialize Rails UJS
-Rails.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+// Include custom JavaScript files
+import "bootstrap";
+import "../stylesheets/application";
+import "controllers";
+
+// You can add more custom JavaScript here
+
+// Example: Adding an event listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello from application.js!");
+});
