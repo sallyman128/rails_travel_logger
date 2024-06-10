ad68cc6e458b65f5dd59a2ea5560f81344ea1fc9
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-10 11:52:32 -0400

    update activestorage and ujs imports

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.0-3/lib/assets/compiled/rails-ujs.js"
-pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.4/lib/assets/compiled/activestorage.js"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.1.3
