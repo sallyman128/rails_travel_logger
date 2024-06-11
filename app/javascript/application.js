f715c3757440d98ff9e735761e886aa5390f049c
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-11 15:44:09 -0400

    resolve all import maps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
-import { Turbo } from "@hotwired/turbo-rails"
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "controllers"
 import "@hotwired/turbo-rails"
+import '@popperjs/core'
+import "popper"
+import "bootstrap"
 
 // Initialize Rails UJS and ActiveStorage
 Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,10 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.0-3/lib/assets/compiled/rails-ujs.js"
-pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.0-2/lib/assets/compiled/activestorage.js"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.1.3
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
