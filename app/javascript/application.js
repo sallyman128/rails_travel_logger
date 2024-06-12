719e515b3fd5527a2e382273b4b4cf7935844944
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-12 16:52:01 -0400

    controllerjs pervents dropdown for some reason

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,8 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
-import "controllers"
+// import "controllers"
 import "@hotwired/turbo-rails"
 import "bootstrap"
 
-// Initialize Rails UJS and ActiveStorage
 Rails.start()
 ActiveStorage.start()
-
-document.addEventListener('turbo:load', () => {
-  const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-  const dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-    return new bootstrap.Dropdown(dropdownToggleEl)
-  })
-})
