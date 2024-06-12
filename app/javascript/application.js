a91b50511e57ddb149507edf2e9f0dedc21d74bf
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-12 14:37:45 -0400

    remove popperjs and popper imports

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "controllers"
 import "@hotwired/turbo-rails"
+import "bootstrap"
 
 // Initialize Rails UJS and ActiveStorage
 Rails.start()
