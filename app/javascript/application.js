2bed49e1a4f74197dee563e3f336dc80ee3ddbd6
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-06-12 16:54:28 -0400

    create placeholder file for mapbox

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import * as ActiveStorage from "@rails/activestorage"
 // import "controllers"
 import "@hotwired/turbo-rails"
 import "bootstrap"
+import "./mapbox/map.js"
 
 Rails.start()
 ActiveStorage.start()
