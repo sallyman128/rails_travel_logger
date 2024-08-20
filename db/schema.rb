802b9c1f481c2a2e31727862c1d869f0b51dfe00
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-08-20 13:29:42 -0400

    reverted changes to no longer use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_05_200820) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "flights", force: :cascade do |t|
     t.string "flight_number"
@@ -20,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_200820) do
     t.datetime "arrival_time"
     t.string "origin"
     t.string "destination"
+    t.bigint "user_id", null: false
+    t.bigint "itinerary_id", null: false
     t.bigint "user_id", null: false
     t.bigint "itinerary_id", null: false
     t.datetime "created_at", null: false
@@ -34,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_200820) do
 
   create_table "itineraries", force: :cascade do |t|
     t.string "name"
+    t.bigint "user_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
